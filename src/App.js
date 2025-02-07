fe236345b11cccecdcb482cf29617cd16cbc7e1c
Zahid Hasan <github@zahidhasan.com.bd>
2025-02-07 22:36:48 +0600

    a

--- src/App.js
+++ src/App.js
@@ -88,16 +88,16 @@ function Introduction() {
     <ChatGPTCodeBox
       language="html"
       code={`
-        <!DOCTYPE html>
-        <html>
-          <head>
-            <title>Page Title</title>
-          </head>
-          <body>
-            <h1>Welcome to My Website</h1>
-            <p>This is a paragraph of text.</p>
-          </body>
-        </html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Page Title</title>
+  </head>
+  <body>
+    <h1>Welcome to My Website</h1>
+    <p>This is a paragraph of text.</p>
+  </body>
+</html>
         `}
       />
 
